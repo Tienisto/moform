11d91ab2411f92c1b6dcdf42ec73e87320ad7966
Tien Do Nam <dev.tien.donam@gmail.com>
2024-04-18 01:02:36 +0200

    docs: update FirstFrameCallback mixin

--- lib/src/utils/first_frame_callback.dart
+++ lib/src/utils/first_frame_callback.dart
@@ -1,12 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:meta/meta.dart';
 
 @internal
 mixin FirstFrameCallback<T extends StatefulWidget> on State<T> {
   bool _firstFrame = true;
 
-  /// Calls the [callback] ONLY the first frame AFTER [initState].
-  /// Should be called in [build] method.
+  /// Calls the [callback] ONLY on the first frame **after** [initState].
+  /// Should be called inside the [build] method.
   void onFirstFrame(void Function() callback) {
     if (_firstFrame) {
       callback();
